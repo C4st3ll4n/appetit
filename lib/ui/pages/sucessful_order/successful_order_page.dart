a9522c71e27389e50ef7d22880e00ec2d3682c85
Pedro Henrique <p13dr0h@gmail.com>
2021-01-26 21:28:18 -0300

    success page OK

--- lib/ui/pages/sucessful_order/successful_order_page.dart
+++ lib/ui/pages/sucessful_order/successful_order_page.dart
@@ -1,19 +1,29 @@
 import 'package:fappetite/ui/pages/sucessful_order/components/components.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SuccessfulOrderPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              Container(
-                child: Image.asset("assets/icon/android.png"),
-              ),
-              NewOrderButton()
-            ],
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Image.asset("lib/ui/assets/chef.png"),
+                ),
+                Text("Pedido realizado !"),
+                NewOrderButton(onTap: (){
+                  Get.offAllNamed("/new_order", );
+                },)
+              ],
+            ),
           ),
         ),
       ),
